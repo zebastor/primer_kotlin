58b31e87dd1e3f2fb36661423d2877a23eaee6b3
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2025-01-11 15:58:09 -0500

    listas

--- src/main/kotlin/listas.kt
+++ src/main/kotlin/listas.kt
@@ -19,4 +19,20 @@ fun main(){
 
     listavacia.removeIf{caracteres -> caracteres.length > 1}
     println(listavacia)
+
+    //ordenar listas
+    var loteria = listOf(23,44,12,10,99)
+    val loteriaSort = loteria.sorted()
+    println(loteriaSort)
+
+    loteria = loteria.shuffled()
+    println("Lista random $loteria")
+
+    // convertir lista de enteros a string
+
+    val loteriaString = loteria.map { numero -> "$numero num" }
+    println(loteriaString)
+
+    val loteriaFiltrada = loteria.filter { numero -> numero > 30 }
+    println(loteriaFiltrada)
 }
